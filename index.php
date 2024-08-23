f2956d7cc6c0a4dd8f81cd02360f442d37bf5201
alessandrofogliada <fogliada.a@gmail.com>
2024-08-23 12:30:16 +0200

    1.0.1 sviluppo milestone 1

--- index.php
+++ index.php
@@ -1,64 +1,94 @@
-<?php 
+<?php
 
-// la variabile mi restituisce la lunghezza della password decisa dall'utente 
-$lenght_password = $_GET['lenght'];
-var_dump ($lenght_password);
-// creo una funzione che mi generi una password random della lunghezza indicata dell'utente 
+// Imposto i caratteri minimo e massimo per la lunghezza della password 
+$min = 8;
+$max = 32;
 
+// creo un array con all'interno tutti i caratteri di cui ho bigogno per creare la password 
+$listChars = [
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    '0123456789',
+    "!?$%$0^+-*/（)［］イのキ="
+];
 
+// salvo in una variabile tutti i caratteri 
+$allChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?$%$0^+-*/()［］イのキ=';
 
-function passwordGenerator(){
-    for($i = 0 ; $i <= $lenght_password ; $i++) {
+// salviamo in una variabile il numero che l'utente ha scelto 
+$lunghezza_password = $_GET ["lunghezzaPassword"];
 
-        // creo una variabile dove saranno inseriti i caratteri random 
-        $password_assembled = [];
+// Con una funzione utilizzeremo questo dato (lunchezza_password) per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
 
-        // genero un numero random 
-        $new_number = rand(1 , 56);
-        // lo inserisco nell'array creato sopra 
-        $password_assembled [] = $new_number;
-        //   var_dump($new_number);
+function generaPassword ($allChars , $lunghezza_password){
+    $psw = '';
 
-        // genero una lettera random 
-        $new_letter = chr(rand(65,90));
-        // lo inserisco nell'array creato sopra 
-        $password_assembled [] = $new_letter;
-
-        var_dump($password_assembled);
+    while(strlen($psw) < $lunghezza_password){
+        $chars = getChars($allChars);
+        $psw .= $chars;
     }
+    return $psw;
+};
+
+function getChars($allChars){
+    return $allChars [rand(0, strlen($allChars) -1 )];
+};
+
+
+// controllo che nell'input dato dall'utente ci sia dentro la l
+
+if(isset ($_GET ["lunghezzaPassword"]) && !empty ($_GET ["lunghezzaPassword"])){
+    // var_dump($_GET ["lunghezzaPassword"]);
+    // creo un altra condizione per vedere se la lunghezza della password rispetta i criteri 
+    if (($_GET ["lunghezzaPassword"]) < $min || ($_GET ["lunghezzaPassword"]) > $max){
+        $output = "Errore la Password deve avere dagli $min ai $max caratteri";
+    } else {
+        $psw = generaPassword ($allChars , $_GET["lunghezzaPassword"]);
+        $output = 'la password generata è' .htmlspecialchars($psw);
+    }
+}else{
+        $output = "Genera una Password con almeno $min fino a $max";
 }
 
+
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP STRONG PASSWORD GENERATOR</title>
+    <title>Generatore di password</title>
 </head>
 <body>
 
-<h1>PHP STRONG PASSWORD GENERATOR</h1>
+    <div>
+        <h1>
+           Generatore di Password
+        </h1>
+    </div>
 
+    <form action="index.php" method="GET">
+        <input type="number" min="1" max="50" name="lunghezzaPassword">
+        <button>Invia</button>
+    </form>
 
-
-<form action="index.php" method="GET">
-    <h4>Scegli un numero da 8 a 32</h4>
-    <input type="number" id="number" name="lenght" min="8" max="32">
-    <button type="submit">Invia</button>
-</form>
-
-    
+    <h3>
+        <?php echo $output?>
+    </h3>
 </body>
 </html>
 
-<!-- Milestone 1
+
+<!-- Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
 Creare un form che invii in GET la lunghezza della password. 
-
 Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
 
-Scriviamo tutto (logica e layout) in un unico file index.php -->
-
-<!-- Milestone 2
+Milestone 2
 Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale -->
